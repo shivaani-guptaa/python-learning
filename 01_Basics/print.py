722afb9a6ba3ee8014581b1527b623a82cb0cb70
shivaani-guptaa <shivaaniguptaa1@gmail.com>
2026-08-03 16:07:31 +0530

    Practice Python basics including print, comments, data types, and operators

--- 01_Basics/print.py
+++
@@ -1,9 +0,0 @@
-
-print("2026", "08", "01", sep="/")
-print("Apple", "Banana", "Mango", sep=", ")
-print("HTML", "CSS", "JavaScript", sep=" -> ")
-print("Python", "Learning", "Journey", sep=" | ")
-print("A", "B", "C", sep="***")
-print("Python", "Java", "C++")
-print("Python", "Java", "C++", sep=", ")
-print("Python", "Java", "C++", sep="-")
