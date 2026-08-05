8357e3b982b719a1ae58e343629767f5f284a1a6
shivaani-guptaa <shivaaniguptaa1@gmail.com>
2026-08-05 23:03:46 +0530

    Learn Python variables and data types with practice examples

---
+++ 01_Basics/practice Program.py
@@ -0,0 +1,143 @@
+# name = "shivani"
+# age = 20
+# price = 25.839
+# age2=age
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+#Arthmetic operators
+# a = 45
+# b = 34
+# print(a+a)
+# print(a-b)
+# print(a * b)
+# print (a/b)
+# print(a%b) # remainder
+# print (a**b) # a^b
+
+#Relational operators 
+# a=40
+# b=40
+# print(a== b)# false
+# print( a!= b) # != is not equal to # true
+# print (a>=b) #true
+# print (a> b) # true
+# print(a<=b)# false
+# print(a <b) #false
+
+# Assignment operators 
+# num =10
+# num += 10
+# num -= 3
+# num *= 4
+# num /= 8
+# print("num:", num)
+
+#Logica operators
+# a = 34
+# b =34
+# print(not False)
+# print(not a>b)
+
+# val1 =True 
+# val2 = False
+# print("and operator:", val1 and val2)
+# print("or operator:", val1 and val2)
+# print("or operator:", (a== b) or (a>b)) 
+
+# type conversion 
+# a= 2
+# b=45
+
+# print(type(a))
+# print(a+b)
+
+# input from user 
+
+# name  = input ("enter your name :")
+# age =("enter your age :")
+# marks = ("enter marks:")
+
+# print("welcpme " , name)
+# print ("age", age )
+# print ("marks", marks)
+
+# home work
+# num1 = input("enter first number :")
+# num2 = input (" enter second number :")
+
+# print ("sum = ",num1 +num2) 
+
+# area of square
+# a = float(input ( "enter 1st number:" ))
+# b = float(input ( "enter 2nd number:" ))
+
+# print ("average=", (a+b)/2)
+
+
+
+name = "Shivani"
+age = 20
+course = "BCA"
+
+print("Name :", name)
+print("Age :", age)
+print("Course :", course)
+
+
+print("\n------------------------------\n")
+
+
+
+#STUDENT PROFILE
+
+name = "Shivani"
+age = 20
+course = "BCA"
+cgpa = 8.7
+city = "Delhi"
+is_pass = True
+
+print("===== STUDENT PROFILE =====")
+print("Name :", name)
+print("Age :", age)
+print("Course :", course)
+print("CGPA :", cgpa)
+print("City :", city)
+print("Pass :", is_pass)
+
+
+print("\n------------------------------\n")
+
+
+#TYPE CHECKING
+
+
+a = 100
+b = 99.99
+c = "Python"
+d = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+print("\n------------------------------\n")
+#  VARIABLE UPDATE
+
+
+salary = 15000
+
+print("Old Salary :", salary)
+
+salary = 25000
+
+print("Updated Salary :", salary)
+
+
+
+
+
