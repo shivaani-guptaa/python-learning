51dd4436318b1b0c03ffec6bee9f3231fd049516
shivaani-guptaa <shivaaniguptaa1@gmail.com>
2026-08-17 14:20:44 +0530

    Learned Python Tuples: creation, indexing, slicing, negative indexing, count and index

--- 01_Basics/Data structure/tuple.py
+++ 01_Basics/Data structure/tuple.py
@@ -4,15 +4,15 @@
 # List → [] → change kar sakte hain
 # Tuple → () → normally change nahi kar sakt
 
-numbers = (10, 20, 30, 40)
+# numbers = (10, 20, 30, 40)
 
-print(numbers)
+# print(numbers)
 
-numbers = (10, 20, 30)
+# numbers = (10, 20, 30)
 
-numbers[0] = 100
+# numbers[0] = 100
 
-print(numbers)
+# print(numbers)
 
 
 # negative indexing 👇
@@ -31,3 +31,56 @@ print(numbers[1:4])
 numbers = (10, 20, 30, 40, 50)
 
 print(numbers[2:])
+
+# count() 👇
+numbers = (10, 20, 10, 30, 10, 40)
+
+print(numbers.count(10))
+
+# index()
+fruits = ("apple", "mango", "banana", "mango", "orange")
+
+print(fruits.index("mango"))
+
+# Tuple Unpacking 🔹
+fruits = ("apple", "mango", "banana")
+
+a, b, c = fruits
+
+print(a)
+print(b)
+print(c)
+
+# * unpacking
+
+fruits = ("apple", "mango", "banana", "orange", "grapes")
+
+a, *b = fruits
+
+print(a)
+print(b)
+# question 
+
+
+fruits = ("apple", "mango", "banana", "orange", "grapes")
+
+a, *b, c = fruits
+
+print(a)
+print(b)
+print(c)
+# single-element tuple.
+
+a = (10)   #Ye tuple nahi, simple integer hai:
+b = (10,)    #Ye tuple hai 
+
+print(type(a))
+print(type(b))
+
+a = ("Python")
+b = ("Python",)
+
+print(type(a))
+print(type(b))
+
+
