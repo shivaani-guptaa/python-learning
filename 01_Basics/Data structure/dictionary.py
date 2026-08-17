03f4af62368cd2ab04a6e6695ecf1ff509b0b76f
shivaani-guptaa <shivaaniguptaa1@gmail.com>
2026-08-17 18:39:48 +0530

    Completed Python Dictionaries with methods and comprehension

--- 01_Basics/Data structure/dictionary.py
+++ 01_Basics/Data structure/dictionary.py
@@ -101,3 +101,170 @@ student = {
 
 print(student.items())
 
+# get() Method
+# get() dictionary mein kisi key ki value access karta hai.
+student = {
+    "name": "Shivani",
+    "age": 20
+}
+
+print(student.get("name"))
+
+# ⭐ [] aur get() mein difference
+# Agar key dictionary mein hai:
+print(student["age"])
+print(student.get("age"))
+
+# Lekin agar key nahi hai:
+
+print(student.get("city"))  #get() error nahi deta. 
+
+# get() mein default value bhi de sakti ho.
+
+person = {
+    "name": "Rahul",
+    "age": 21
+}
+
+print(person.get("city", "Not Found"))
+
+# pop()  : pop() dictionary se kisi key-value pair ko remove karta hai.
+
+fruits = {
+    "a": "apple",
+    "b": "banana",
+    "c": "cherry"
+}
+
+fruits.pop("b")
+
+print(fruits)
+
+# 🔹 popitem()
+# popitem() dictionary ka last key-value pair remove karta hai.
+person = {
+    "name": "Rahul",
+    "age": 21,
+    "city": "Delhi"
+}
+
+person.popitem()
+
+print(person)
+
+
+# clear() poori dictionary empty kar deta hai.
+
+fruits = {
+    "a": "apple",
+    "b": "banana"
+}
+
+fruits.clear()
+
+print(fruits)
+
+# {} → Empty dictionary
+# [] → Empty list
+# () → Empty tuple
+
+
+
+# update() se dictionary mein multiple key-value pairs add ya existing values update kar sakte hain.
+student = {
+    "name": "Shivani",
+    "age": 20
+}
+
+student.update({
+    "age": 21,
+    "course": "BCA"
+})
+
+print(student)
+
+person = {
+    "name": "Rahul",
+    "age": 21
+}
+
+person.update({
+    "age": 22,
+    "city": "Delhi"
+})
+
+print(person)
+# in se check kar sakte hain ki koi key dictionary mein exist karti hai ya nahi.
+person = {
+    "name": "Rahul",
+    "age": 21,
+    "city": "Delhi"
+}
+
+print("city" in person)
+print("phone" in person)
+
+# 🔹 not in
+student = {
+    "name": "Shivani",
+    "course": "BCA"
+}
+
+print("age" not in student)
+print("name" not in student)
+
+# 🔹 fromkeys()
+
+# fromkeys() se multiple keys wali new dictionary bana sakte hain.
+keys = ("name", "age", "city")
+
+student = dict.fromkeys(keys, "Unknown")
+
+print(student)
+#question
+
+keys = ("a", "b", "c")
+
+data = dict.fromkeys(keys, 0)
+
+print(data)
+
+# 🔹 Dictionary Comprehension
+
+numbers = [1, 2, 3, 4]
+
+square = {}
+
+for n in numbers:
+    square[n] = n * n
+
+print(square)
+
+# 2nd method
+numbers = [1, 2, 3, 4]
+
+square = {n: n * n for n in numbers}
+
+print(square)
+#que
+numbers = [1, 2, 3]
+
+result = {n: n + 10 for n in numbers}
+
+print(result)
+
+# que dic with condition
+numbers = [1, 2, 3, 4, 5, 6]
+
+even = {n: n * n for n in numbers if n % 2 == 0}
+
+print(even)
+
+# que 
+numbers = [1, 2, 3, 4, 5]
+
+result = {n: n * 2 for n in numbers if n % 2 != 0}
+
+print(result)
+
+
